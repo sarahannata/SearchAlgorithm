6de7c70c75e2035173c6a25a1435e0d7ab57a08b
sarahannata <sarahannata@gmail.com>
2022-10-26 16:44:30 +0700

    Menambahkan if else

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -60,7 +60,14 @@ namespace SearchAlgorithm
                     mid = (lowerbound + upperbound) / 2;
                     ctr++;
                 }
-                
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparison : " + ctr);
+
+                Console.Write("\nContinue Search (y/n): ");
+                ch = char.Parse(Console.ReadLine().ToUpper();
             }
         }
     }
