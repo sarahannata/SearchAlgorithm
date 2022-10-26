7578f5e5c47d11d67755715da146267a586edb27
sarahannata <sarahannata@gmail.com>
2022-10-26 16:16:48 +0700

    Menambahkan function while

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -13,7 +13,16 @@ namespace SearchAlgorithm
 
         public void input()
         {
-            
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();  
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element. \n");
+            }
         }
     }
 }
